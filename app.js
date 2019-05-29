294f5ec8396113980a3f8fc14046afea5854c65f
Barry7 <cgfbchooo@163.com>
2019-05-29 14:51:48 +0800

    添加新的gitignore规则、 添加客户端渲染示例

---
+++ app.js
@@ -0,0 +1,9 @@
+//webpack打包入口
+import Vue from 'vue';
+import App from './App.vue';
+var app = new Vue({
+    el: '#app',
+    components: {
+        App
+    }
+});
---
+++ components/Bar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='foo'>
+        <h1>Bar</h1>
+        <p>Component </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Bar"
+    }
+</script>
+
+<style scoped>
+    .foo{
+        background: blue;
+    }
+</style>
